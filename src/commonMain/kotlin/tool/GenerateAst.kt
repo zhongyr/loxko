c8aeb6da2aabf6e1a621befd04e5374b9062e54f
zhongyr <zhongyr@g.clemson.edu>
2024-01-12 12:36:01 +0800

    feat: add ast generator

--- src/commonMain/kotlin/tool/GenerateAst.kt
+++ src/commonMain/kotlin/tool/GenerateAst.kt
@@ -15,7 +15,24 @@ fun defineType(buffer: Buffer, baseName: String, className: String, fieldList: S
   fields.map { field ->
     buffer.writeString("val ${field.second} : ${field.first},")
   }
-  buffer.writeString(") : $baseName() {} \n")
+  buffer.writeString(") : $baseName() { \n")
+  buffer.writeString("""
+    override fun<R> accept(visitor: Visitor<R>) : R {
+      return visitor.visit$className$baseName(this)
+    }
+  }
+  """.trimIndent())
+}
+
+fun defineVisitor(buffer: Buffer, baseName: String, types: List<String>) {
+  buffer.writeString("  interface Visitor<R> {\n")
+
+  types.map {
+    val typeName = it.split(":")[0].trim()
+    buffer.writeString("    fun visit$typeName$baseName(${baseName.lowercase()} : $typeName) : R\n")
+  }
+
+  buffer.writeString("  }\n")
 }
 
 fun defineAst(outputDir: String, baseName: String, types: List<String>) {
@@ -35,13 +52,22 @@ companion object {
       val (className, fields) = type.split(":").map { it.trim() }
       defineType(this, baseName, className, fields)
     }
-
+    // companion object
     this.writeString(
       """
       }
+      
+    """.trimIndent())
+
+    defineVisitor(this, baseName, types)
+    this.writeString("\n")
+    this.writeString("  abstract fun<R> accept(visitor: Visitor<R>) : R \n")
+    // class
+    this.writeString(
+      """
       }
-    """.trimIndent()
-    )
+    """.trimIndent())
+
   }.let { buffer ->
     SystemFileSystem.sink(Path(path)).write(buffer, buffer.size)
   }
